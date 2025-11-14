591ff78e7dfa93b674ccd4a35247e5c6cc946cbf
share-a-byte <write2vaniramesh@gmail.com>
2025-11-13 18:21:35 -0600

    first comm

---
+++ Makefile
@@ -0,0 +1,4 @@
+CC = clang++
+
+main.o: main.cpp
+	$(CC) -c main.cpp -o main.o
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+        // Code to be executed, potentially using command-line arguments
+    std::cout << "i did it!" << std::endl;
+    return 0;
+}
